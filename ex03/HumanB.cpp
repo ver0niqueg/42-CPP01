f2d8c84340ac1705bd028f263578f32e14be610b
Veronique Galmiche <vgalmich@f1r8s3.clusters.42paris.fr>
2025-07-31 14:45:02 +0200

    ex03 cleaned

--- ex03/HumanB.cpp
+++ ex03/HumanB.cpp
@@ -6,7 +6,7 @@
 /*   By: vgalmich <vgalmich@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/28 18:11:21 by vgalmich          #+#    #+#             */
-/*   Updated: 2025/07/29 19:11:13 by vgalmich         ###   ########.fr       */
+/*   Updated: 2025/07/31 14:44:30 by vgalmich         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,13 +33,9 @@ void HumanB::setWeapon(Weapon &weapon)
 void HumanB::attack() const
 {
     if (_weaponPTR)
-    {
         std::cout << _name << " attacks with their " << _weaponPTR->getType() << std::endl;
-    }
     else
-    {
         std::cout << _name << " has no weapon" << std::endl;
-}
 }
 
 
